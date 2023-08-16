07382b85505be18e09e7359d185ff7d73be26988
Tony An <anxuanzi@sh-c.cn>
2023-08-16 00:27:40 -0700

    "Implement new logging functionality with jog.go"
    The commit introduces a new jog.go file that adds a comprehensive logging utility to our project, utilizing Zap logs for efficient structured logging. This solution provides different log levels based on development or production settings. Files go.mod and go.sum have also been updated with required dependencies zap and multierr from Uber.

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/uiucjfo/jog
 
 go 1.20
+
+require (
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.25.0 // indirect
+)
---
+++ jog.go
@@ -0,0 +1,145 @@
+package jog
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type JLog struct {
+	ZapLogger *zap.Logger
+	ZapSugar  *zap.SugaredLogger
+}
+
+type LogLevel int
+
+const (
+	LevelProd LogLevel = iota
+	LevelDev
+)
+
+var instance *JLog
+
+func New(level ...LogLevel) *JLog {
+	if len(level) == 0 {
+		level[0] = LevelDev
+	}
+	jl := &JLog{}
+	zapCfg := zap.NewProductionConfig()
+	if level[0] == LevelDev {
+		zapCfg = zap.NewDevelopmentConfig()
+		zapCfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	}
+	jl.ZapLogger, _ = zapCfg.Build()
+	jl.ZapSugar = jl.ZapLogger.Sugar()
+	instance = jl
+	return jl
+}
+
+func GetLogger() *zap.Logger {
+	if instance == nil {
+		New()
+	}
+	return instance.ZapLogger
+}
+
+func GetSugar() *zap.SugaredLogger {
+	if instance == nil {
+		New()
+	}
+	return instance.ZapSugar
+}
+
+func (jl *JLog) Close() {
+	jl.ZapLogger.Sync()
+}
+
+func Close() {
+	if instance == nil {
+		return
+	}
+	instance.Close()
+}
+
+func Debug(args ...interface{}) {
+	if instance == nil {
+		New()
+	}
+	instance.ZapSugar.Debug(args...)
+}
+
+func Debugf(template string, args ...interface{}) {
+	if instance == nil {
+		New()
+	}
+	instance.ZapSugar.Debugf(template, args...)
+}
+
+func Info(args ...interface{}) {
+	if instance == nil {
+		New()
+	}
+	instance.ZapSugar.Info(args...)
+}
+
+func Infof(template string, args ...interface{}) {
+	if instance == nil {
+		New()
+	}
+	instance.ZapSugar.Infof(template, args...)
+}
+
+func Warn(args ...interface{}) {
+	if instance == nil {
+		New()
+	}
+	instance.ZapSugar.Warn(args...)
+}
+
+func Warnf(template string, args ...interface{}) {
+	if instance == nil {
+		New()
+	}
+	instance.ZapSugar.Warnf(template, args...)
+}
+
+func Error(args ...interface{}) {
+	if instance == nil {
+		New()
+	}
+	instance.ZapSugar.Error(args...)
+}
+
+func Errorf(template string, args ...interface{}) {
+	if instance == nil {
+		New()
+	}
+	instance.ZapSugar.Errorf(template, args...)
+}
+
+func Fatal(args ...interface{}) {
+	if instance == nil {
+		New()
+	}
+	instance.ZapSugar.Fatal(args...)
+}
+
+func Fatalf(template string, args ...interface{}) {
+	if instance == nil {
+		New()
+	}
+	instance.ZapSugar.Fatalf(template, args...)
+}
+
+func Panic(args ...interface{}) {
+	if instance == nil {
+		New()
+	}
+	instance.ZapSugar.Panic(args...)
+}
+
+func Panicf(template string, args ...interface{}) {
+	if instance == nil {
+		New()
+	}
+	instance.ZapSugar.Panicf(template, args...)
+}
